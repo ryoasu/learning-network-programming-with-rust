5d63a949ef4ef842cf3d1f6518474bb28f68201b
ryoasu <ryoasu107@gmail.com>
2020-04-04 00:20:36 +0900

    :sparkles: Add TCP echo server and client

---
+++ socket-programming/src/main.rs
@@ -0,0 +1,66 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+// mod udp_client;
+// mod udp_server;
+
+fn main() {
+  env::set_var("RUST_LOG", "debug");
+  env_logger::init();
+  let args: Vec<String> = env::args().collect();
+  if args.len() != 4 {
+    error!("Please specify [tcp|udp] [server|client] [addr:port]");
+    std::process::exit(1);
+  }
+
+  let protocol: &str = &args[1];
+  let role: &str = &args[2];
+  let address: &str = &args[3];
+
+  match protocol {
+    "tcp" => match role {
+      "server" => {
+        tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+      }
+      "client" => {
+        tcp_client::connect(address).unwrap_or_else(|e| error!("{}", e));
+      }
+      _ => {
+        missing_role();
+      }
+    },
+    "udp" => match role {
+      "server" => {
+        // TODO: UDP Server
+      }
+      "client" => {
+        // TODO: UDP Client
+      }
+      _ => {
+        missing_role();
+      }
+    },
+    _ => {
+      missing_protocol();
+    }
+  }
+}
+
+/**
+ * protocolが不正な時にエラーを出す関数
+ */
+fn missing_protocol() {
+  error!("Please specify tcp or udp on the 1st argument");
+  std::process::exit(1);
+}
+
+/**
+ * roleが不正な時にエラーを出す関数
+ */
+fn missing_role() {
+  error!("Please specify tcp or udp on the 1st argument");
+  std::process::exit(1);
+}
